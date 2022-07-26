455f42a1e70b0f1b1cae85e9157f5f3b834cae66
webpojectitera123 <web.itera@gmail.com>
2022-07-26 23:18:45 +0700

    selasa

--- src/lib/config/firebase.js
+++ src/lib/config/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const appConfig = {
-  apiKey: "AIzaSyAIF0h0NSEotP5jCINdl__wyPaa3NWlXBw",
-  authDomain: "misdar-blog.firebaseapp.com",
-  projectId: "misdar-blog",
-  storageBucket: "misdar-blog.appspot.com",
-  messagingSenderId: "327499720236",
-  appId: "1:327499720236:web:7049c883ff0eb8c8791389"
+ 
 };
 
 const app = initializeApp(appConfig);
